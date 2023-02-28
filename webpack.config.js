fbf86b61092588b5868dd94aff236fc90d2f1dbc
slavaMalenko <denouement20@yandex.ru>
2023-02-28 23:02:24 +0300

    Настройка конфигурации webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,34 @@
-module.exports = {
+const path = require('path');
+const webpack = require('webpack');
 
-}
+const paths = {
+    src: path.resolve(__dirname, 'src'),
+    dist: path.resolve(__dirname, 'dist')
+};
+
+module.exports = {
+    context: paths.src, // базовая директория для точек входа и загрузчиков    
+    entry: {
+        app: './index'  // точка входа в приложение, наш src/index.ts файл, названием итогового бандла будет имя свойства - app
+    },
+
+    output: {
+        path: paths.dist,  // путь для результатов сборки 
+        filename: '[name].bundle.js'  // название итогового бандла, получится dist/app.bundle.js
+    },
+
+    resolve: {
+        extensions: ['.ts'] // указание расширений файлов, которые webpack будет обрабатывать, и пытаться добавить автоматически (например получив запрос на index, не найдет его и попробует index.ts)
+    },
+
+    devtool: 'inline-source-map',
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader'
+            } // загрузчик для обработки файлов с расширением .ts
+        ]
+    }
+};
